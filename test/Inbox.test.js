18f7ed412fc6ad14dde1dc45a014a6af07c30088
Christian Pojoni <dev@pojoni.at>
2019-11-17 20:12:27 +0100

    await web3

--- test/Inbox.test.js
+++ test/Inbox.test.js
@@ -13,7 +13,7 @@ beforeEach(async () => {
 
   // use one of those accounts to deploy
   // the contract
-  inbox = new web3.eth.Contract(JSON.parse(interface))
+  inbox = await new web3.eth.Contract(JSON.parse(interface))
     .deploy({ data: bytecode, arguments: ['Hi there'] })
     .send({ from: accounts[0], gas: '1000000' });
 });
